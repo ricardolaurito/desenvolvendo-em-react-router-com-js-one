d9150679dc4df13ef66f2e970b4003a25a464646
Ricardo Laurito <ricardolaurito@yahoo.com>
2023-09-26 21:56:35 -0500

    Refatorando

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "componentes/Post";
-import NaoEncontrada from "componentes/NaoEncontrada";
+import NaoEncontrada from "paginas/NaoEncontrada";
 
 function AppRoutes() {
   return (
@@ -14,14 +14,11 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
-          <Route path="sobremim" element={<SobreMim />} />
-          <Route path="posts/:id" element={<Post />} />
+          <Route path="sobremim" element={<SobreMim />} />          
         </Route>
 
-        <Route
-          path="*"
-          element={< NaoEncontrada />}
-        />
+        <Route path="posts/:id" element={<Post />} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
       <Rodape />
